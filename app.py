de8d153e8375fc7c2b171bc0bc1dd91da91202e5
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-04-20 17:09:47 +0100

    Added app.config for secret key and MongoDB

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ if path.exists('env.py'):
     import env
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 @app.route(/)
 def hello():
