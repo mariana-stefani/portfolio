08335d04804c8130db0d1184a4efff7e72a976b8
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-04-20 17:06:35 +0100

    Created basic Flask application

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+import os
 from flask import Flask, render_template
 
 if path.exists('env.py'):
     import env
+
+app = Flask(__name__)
+
+@app.route(/)
+def hello():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
