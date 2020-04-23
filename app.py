a52687f47c5e2c2f1ee2d951f01b7922de39f721
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-04-23 19:03:40 +0100

    Added skills collection to index route

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    skills = mongo.db.skills.find()
+    return render_template('index.html', skills=skills)
 
 
 if __name__ == '__main__':
