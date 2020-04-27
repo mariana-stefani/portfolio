a3425b3e75c43553b6095cbe9d6a4a123e0e4a08
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-04-27 18:23:48 +0100

    Added function to validate and send email

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,51 @@
+//Progress Circle
 // Tutorial from https://bootstrapious.com/p/circular-progress-bar
 
-$(function() {
+$(function () {
+  $(".progress").each(function () {
+    var value = $(this).attr("data-value");
+    var left = $(this).find(".progress-left .progress-bar");
+    var right = $(this).find(".progress-right .progress-bar");
+    var bar = $(this).find(".progress");
 
-    $(".progress").each(function() {
-  
-      var value = $(this).attr('data-value');
-      var left = $(this).find('.progress-left .progress-bar');
-      var right = $(this).find('.progress-right .progress-bar');
-      var bar = $(this).find('.progress')
-  
-      if (value > 0) {
-        if (value <= 50) {
-          right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-        } else {
-          right.css('transform', 'rotate(180deg)')
-          left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
-        }
+    if (value > 0) {
+      if (value <= 50) {
+        right.css("transform", "rotate(" + percentageToDegrees(value) + "deg)");
+      } else {
+        right.css("transform", "rotate(180deg)");
+        left.css(
+          "transform",
+          "rotate(" + percentageToDegrees(value - 50) + "deg)"
+        );
       }
-  
-    })
-  
-    function percentageToDegrees(percentage) {
-  
-      return percentage / 100 * 360
-  
-    }   
-  
+    }
   });
+
+  function percentageToDegrees(percentage) {
+    return (percentage / 100) * 360;
+  }
+});
+
+//Email JS
+
+$(function () {
+  emailjs.init("user_ihMVh8GieFlqswQcnywX0");
+})();
+
+function sendMail(contactForm) {
+  email
+    .send("gmail", "mariana", {
+      "from _name": contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      contact_request: contactForm.contactsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("success", response);
+      },
+      function (error) {
+        console.log("failed", error);
+      }
+    );
+  return false;
+}
