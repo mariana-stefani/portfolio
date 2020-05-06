17f9444d995854aa1b016b5af3c9e3bae04504ad
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-06 19:16:07 +0100

    Added project_order to update_project route

--- app.py
+++ app.py
@@ -149,7 +149,8 @@ def update_project(project_id):
                        'project_desc': request.form.get('project_desc'),
                        'project_github': request.form.get('project_github'),
                        'project_live': request.form.get('project_live'),
-                       'project_img': request.form.get('project_img')
+                       'project_img': request.form.get('project_img'),
+                       'project_order': request.form.get('project_order')
                        })
         flash('Your project has been successfully updated.')
         return redirect(url_for('index'))
