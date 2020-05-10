fd3bf69057fbe7721cdafef6486f806ee9de051c
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-10 15:01:17 +0100

    Added min and max length to password

--- forms.py
+++ forms.py
@@ -3,8 +3,10 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 # Login Form
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),
                                                    Length(min=3, max=15)])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     Length(min=3, max=15)])
     submit = SubmitField('Login')
