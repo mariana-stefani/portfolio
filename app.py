95ada165af216df6135d3de4cbef25d2bfe35f41
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-15 11:35:27 +0100

    Updated app.run on app.py

--- app.py
+++ app.py
@@ -173,4 +173,4 @@ def logout():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=(os.environ.get('PORT')),
-            debug=os.environ.get('DEBUG'))
+            debug=False)
